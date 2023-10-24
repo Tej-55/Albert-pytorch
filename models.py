3c0696b643a2fea87c37a8c669222a24dd825c0e
Tejas Agrawal <98448186+Tej-55@users.noreply.github.com>
2023-10-24 18:06:50 +0530

    Update models.py

--- models.py
+++ models.py
@@ -82,6 +82,20 @@ class Embeddings(nn.Module):
         #return self.drop(self.norm(e))
         return self.norm(e)
 
+class LayerNorm(nn.Module):
+    "A layernorm module in the TF style (epsilon inside the square root)."
+    def __init__(self, cfg, variance_epsilon=1e-12):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.ones(cfg.hidden))
+        self.beta  = nn.Parameter(torch.zeros(cfg.hidden))
+        self.variance_epsilon = variance_epsilon
+
+    def forward(self, x):
+        u = x.mean(-1, keepdim=True)
+        s = (x - u).pow(2).mean(-1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.variance_epsilon)
+        return self.gamma * x + self.beta
+
 class MultiHeadedSelfAttention(nn.Module):
     """ Multi-Headed Dot Product Attention """
     def __init__(self, cfg):
