696518bfa62cdfca41fd82c6655d22a8f53ee963
Tejas Agrawal <98448186+Tej-55@users.noreply.github.com>
2023-10-24 16:27:47 +0530

    Update pretrain.py

--- pretrain.py
+++ pretrain.py
@@ -255,7 +255,7 @@ def main(args):
                            global_step)
         return loss_lm + loss_sop
 
-    trainer.train(get_loss, model_file=None, None, data_parallel=True)
+    trainer.train(get_loss, args.model_file, None, args.data_parallel)
 
 
 if __name__ == '__main__':
